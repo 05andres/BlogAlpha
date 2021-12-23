6e57c631337e77501aed32eb765e1be3e419c35b
carlos andres lopez <30788836+05andres@users.noreply.github.com>
2021-12-22 19:24:13 -0500

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles #, only: [:show, :index,:new, :create, :update, :edit, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users , except: [:new]
 end
